eca9ffc247a1562025e69582eb45974d221d611c
havr1985 <gavrtel1985@gmail.com>
2023-11-16 16:20:49 +0200

    ended task

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,44 @@
+class Key {
+    constructor(private signature: number = Math.random()) {};
+    getSignature(): number {
+        return this.signature
+    };
+};
+class Person {
+    private key: Key
+    constructor(key: Key) {
+        this.key = key;
+    };
+    getKey(): Key {
+        return this.key
+    };
+};
+abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.key = key;
+    };
+
+    abstract openDoor(key: Key): void;
+
+    comeIn(person: Person): void{
+        if (this.door) {
+            this.tenants.push(person)
+        };
+    };
+};
+
+class MyHouse extends House{
+    openDoor(key: Key): void {
+        if (this.key.getSignature() === key.getSignature()) {
+            this.door = true;
+        };
+    };
+};
+
 const key = new Key();
 
 const house = new MyHouse(key);
